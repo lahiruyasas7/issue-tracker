c12ac2b362f0cb43c53e9fb80eeef27f9d7a8da5
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-04-24 21:16:16 +0530

    feat: created user register input validation schema

---
+++ server/src/validation-schemas/auth.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .refine((val) => /[A-Z]/.test(val), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .refine((val) => /[!@#$%^&*(),.?":{}|<>]/.test(val), {
+      message: "Password must contain at least one special character",
+    }),
+});
+
+export type RegisterBody = z.infer<typeof registerSchema>;
