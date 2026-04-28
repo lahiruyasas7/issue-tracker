ab83702adf70cc4a7fb788bbaa337139881d4ac0
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-04-28 23:31:07 +0530

    feat: set validation check for query and params

--- server/src/middleware/validate.middleware.ts
+++ server/src/middleware/validate.middleware.ts
@@ -1,11 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodSchema, ZodError } from "zod";
 
+type RequestField = "body" | "query" | "params";
+
 export const validate =
-  (schema: ZodSchema) =>
+  <T>(schema: ZodSchema<T>, field: RequestField = "body") =>
   (req: Request, res: Response, next: NextFunction): void => {
     try {
-      req.body = schema.parse(req.body);
+      const parsed = schema.parse(req[field]);
+
+      //Type-safe assignments
+      if (field === "body") {
+        req.body = parsed;
+      } else {
+        // store safely instead of overriding
+        (req as any).validated = (req as any).validated || {};
+        (req as any).validated[field] = parsed;
+      }
+
       next();
     } catch (err) {
       if (err instanceof ZodError) {
@@ -13,6 +25,7 @@ export const validate =
           field: e.path.join("."),
           message: e.message,
         }));
+
         res.status(422).json({
           success: false,
           message: "Validation failed",
