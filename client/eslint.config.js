9bd13647d2a42109f856497e700eee424961c1a1
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-04-29 15:38:26 +0530

    fix: imported prettierConfig and prettierPlugin

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
